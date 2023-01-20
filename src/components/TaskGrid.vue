6134b25de38a9b5e3d2832708b6f5cb61f5b2b82
80847835 <leonardo.s@compasso.com.br>
2023-01-20 13:05:50 -0300

    feature: component task-grid

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="task-grid">
+        <template v-if="tasks.length">
+            <Task v-for="task in tasks" :key="task.name" :task="task"/>
+        </template>
+        <p v-else class="all-done-tasks">You finish all your tasks :)</p>
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+    components: { Task },
+    props: {
+        tasks: { type: Array, required: true }
+    }
+}
+</script>
+
+<style>
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .task-grid .task {
+        margin: 10px;
+    }
+
+    .all-done-tasks{
+        color: #AAA;
+        font-size: 1.7rem;
+    }
+
+</style>
